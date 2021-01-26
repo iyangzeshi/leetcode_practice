70e5dce3f5d205e73eba8a3fbff3ee8dc0f075a5
iyangzeshi <iyangzeshi@outlook.com>
2021-01-25 22:42:51 -0800

    origin

--- src/leetcode/editor/en/Foo.java
+++ src/leetcode/editor/en/Foo.java
@@ -8,12 +8,21 @@ package leetcode.editor.en;
  Date: 2020-08-06 15:25
  */
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Foo {
 	
 	public static void main(String[] args) {
 		int[] nums1 = new int[] {1, 2};
 		int[] nums2 = new int[] {1, 2};
 		System.out.println(nums1.equals(nums2));
+		int[] nums = {1, 2, 5};
+		Map<Integer, int[]> map = new HashMap<>();
+		map.put(1, nums);
+		nums[2] = 4;
+		System.out.println(Arrays.toString(map.get(1)));
 	}
 	
 
