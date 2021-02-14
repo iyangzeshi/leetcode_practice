8b4b999fcf37180a7911ee81a1e8d8bed23833ab
iyangzeshi <iyangzeshi@outlook.com>
2021-02-14 14:17:43 -0800

    origin

---
+++ src/leetcode/editor/en/Leetcode0516LongestPalindromicSubsequence.java
@@ -0,0 +1,61 @@
+//Given a string s, find the longest palindromic subsequence's length in s. You 
+//may assume that the maximum length of s is 1000. 
+//
+// Example 1: 
+//Input: 
+//
+// 
+//"bbbab"
+// 
+//Output:
+//
+// 
+//4
+// 
+//One possible longest palindromic subsequence is "bbbb".
+//
+// 
+//
+// Example 2: 
+//Input: 
+//
+// 
+//"cbbd"
+// 
+//Output:
+//
+// 
+//2
+// 
+//One possible longest palindromic subsequence is "bb".
+// 
+// Constraints: 
+//
+// 
+// 1 <= s.length <= 1000 
+// s consists only of lowercase English letters. 
+// 
+// Related Topics Dynamic Programming 
+// 👍 2791 👎 212
+
+package leetcode.editor.en;
+
+// 2021-02-14 14:11:25
+// Zeshi Yang
+public class Leetcode0516LongestPalindromicSubsequence{
+    // Java: longest-palindromic-subsequence
+    public static void main(String[] args) {
+        Solution sol = new Leetcode0516LongestPalindromicSubsequence().new Solution();
+        // TO TEST
+        
+        System.out.println();
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
--- src/leetcode/editor/en/Leetcode0992SubarraysWithKDifferentIntegers.java
+++ src/leetcode/editor/en/Leetcode0992SubarraysWithKDifferentIntegers.java
@@ -251,8 +251,8 @@ class Solution1 {
 class Solution2 {
     
     public int subarraysWithKDistinct(int[] A, int K) {
-        Window window1 = new Window();
-        Window window2 = new Window();
+        Window window1 = new Window(); // largest window ends with current location with size K - 1
+        Window window2 = new Window();// largest window ends with current location with size K
         int res = 0;
         int left1 = 0;
         int left2 = 0;
