c4ec0100c1f21943cea159ac80b7cd9f1fbd86e8
iyangzeshi <iyangzeshi@outlook.com>
2023-02-03 15:28:07 -0800

    add comment for LC0005

--- src/leetcode/editor/en/Leetcode0005LongestPalindromicSubstring.java
+++ src/leetcode/editor/en/Leetcode0005LongestPalindromicSubstring.java
@@ -32,44 +32,68 @@ public class Leetcode0005LongestPalindromicSubstring{
         System.out.println(res);
     }
 //leetcode submit region begin(Prohibit modification and deletion)
+/** 面试的时候用 Solution 4*/
+// Solution 4: Expand Around Center
+// T(n) = O(n^2), S(n) = O(1)
+// 13 ms,击败了95.73% 的Java用户, 38.8 MB,击败了82.36% 的Java用户
+/*
+用brute force的做法。在String里面遍历所有的substring，
+开头和结尾都有n种情况，所以有n^2种substring
+然后每个substring，都要遍历完这个substring本身，才能确定是不是palindrome，这里要O(n)
+所以brute force时间复杂度是O(n^3)
+
+但是在brute force的做法里面，我们发现有很多是重复计算的。
+比如说substring(i, j) 和substring(i-1, j+1),前者substring(i, j) 的计算包括了substring(i-1, j+1)
+如果能把这些重复计算避开就好了
+
+发现从中间开始往两边扩展找palindrome比较好，不会有重复计算的地方。一共有2*n - 1个中间点
+ */
 class Solution {
-    
-    public String longestPalindrome(String s) {
-        // corner case
-        if (s == null || s.length() <= 1) {
-            return s;
-        }
-        int len = s.length();
-        int[] res = {0, 1}; // 记录当前最大palindrome的起点,和长度
-        for (int i = 0; i < len; i++) {
-            for (int j = i; j < len; j++) {
-                if (isPalindrome(s, i, j)) {
-                    if (j - i + 1 > res[1]) {
-                        res[1] = j - i + 1;
-                        res[0] = i;
-                    }
-                }
-            }
-        }
-        return s.substring(res[0], res[0] + res[1]);
-    }
-    
-    private boolean isPalindrome(String str, int i, int j) {
-        int start = i;
-        int end = j;
-        while (start < end) { // <= 也可以
-            if (str.charAt(start) != str.charAt(end)) {
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
-    }
-    
+	
+	public String longestPalindrome(String s) {
+		// corner case
+		if (s == null || s.length() <= 1) {
+			return s;
+		}
+		int len = s.length();
+		int[] max = {1}; // 记录当前最大的palindrome的长度
+		int[] start = {0}; // 记录当前最大palindrome的起点
+		
+		for (int i = 0; i < len; i++) {
+			//当剩下的subString的最大长度比max[0]小的时候，就不需要比较了
+			if (2 * Math.min(i + 1, len - i) < max[0]
+				&& 2 * Math.min(i + 1, len - i - 1) < max[0]) {
+				break;
+			}
+			findMax(s, i, i, max, start);
+			findMax(s, i, i + 1, max, start);
+		}
+		// subString的区间是[left, right)
+		return s.substring(start[0], start[0] + max[0]);
+	}
+	
+	// find max length of palindrome centered at [left, right]
+	private void findMax(String s, int left, int right, int[] max, int[] start) {
+		int len = s.length();
+		while (left >= 0 && right < len) {
+			if (s.charAt(left) == s.charAt(right)) {
+				left--;
+				right++;
+			} else {
+				break;
+			}
+		}
+		
+		int dist = right - left - 1;
+		
+		if (dist > max[0]) {
+			max[0] = dist;
+			start[0] = left + 1;
+		}
+	}
+	
 }
 //leetcode submit region end(Prohibit modification and deletion)
-/** 面试的时候用 Solution 4*/
 
 
 // Solution 1: brute force
